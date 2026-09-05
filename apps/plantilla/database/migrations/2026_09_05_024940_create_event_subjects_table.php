01d2bd07a0d11c7003aa63462680b4405545ffe5
fc251370-sys <fc251370@uls.edu.sv>
2026-09-04 21:26:03 -0600

    migraciones echas terminadas

---
+++ apps/plantilla/database/migrations/2026_09_05_024940_create_event_subjects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_subjects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('event_id')->contrained('events')->cascadeOnDelete();
+            $table->foreignId('teacher_id')->contrained('users')->restrictOnDelete();
+            $table->foreignId('carrera_id')->contrained('carreras')->restrictOnDelete();
+            $table->string('subject_name', 150);
+            $table->timestamps('created_at')->useCurrent();
+        });
+    }
+
+    //event_id con cascadeOnDelete si se borra un evento completo que se borren las materias asociadas a ese ciclo
+
+    //teacher_id con restrictOnDelete no deja borrar a un profesor si tiene materias asignadas en algún evento — evita dejar una materia sin docente responsable igual que con coordinador
+
+    //carrera_id con restrictOnDelete lo mism no deja borrar una carrera si tiene materias de evento 
+
+
+
+    
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_subjects');
+    }
+};
